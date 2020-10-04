d55d512127f2d91ba12e9c27969112e577d98eba
Martin Harrigan (ITC) <martin.harrigan@itcarlow.ie>
2020-10-04 10:52:53 +0100

    Add the submodules

---
+++ Assets/Plugins/Modulo3/Modulo3Filter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Modulo3Filter
+{
+    public static int modulo3(int t_input, int t_devisor)
+	{
+		return t_input - (t_devisor * (t_input / t_devisor));
+	}
+
+	public static int[] modulo3s(int[] input)
+	{
+		int[] result = new int[input.Length];
+		for (int i = 0; i < input.Length; i++)
+		{
+			result[i] = modulo3(input[i], 3);
+		}
+		return result;
+	}
+}
